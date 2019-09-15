00a4c34e261685593fb40d5f7012ef9deadcc495
djdisodo <djdisodo@gmail.com>
2019-09-16 00:59:40 +0900

    ajfajf

--- src/protocol/packet_reliability.rs
+++ src/protocol/packet_reliability.rs
@@ -11,6 +11,30 @@ pub enum PacketReliability {
 
 impl PacketReliability {
 	pub fn is_reliable(&self) -> bool {
-		unimplemented!(); //TODO
+		match self {
+			PacketReliability::Reliable => true,
+			PacketReliability::ReliableOrdered => true,
+			PacketReliability::ReliableSequenced => true,
+			PacketReliability::ReliableWithACKReceipt => true,
+			PacketReliability::ReliableOrderedWithACKReceipt => true,
+			_ => false
+		}
+	}
+	pub fn is_sequenced(&self) -> bool {
+		match self {
+			PacketReliability::UnreliableSequenced => true,
+			PacketReliability::ReliableSequenced => true,
+			_ => false
+		}
+	}
+	pub fn is_ordered(&self) -> bool {
+		match self {
+			PacketReliability::ReliableOrdered => true,
+			PacketReliability::ReliableOrderedWithACKReceipt => true,
+			_ => false
+		}
+	}
+	pub fn is_sequence_or_ordered(&self) -> bool {
+		return self.is_sequenced() || self.is_ordered();
 	}
 }
